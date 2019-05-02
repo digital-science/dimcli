5e3f52ff0cd876fa60d6598854d0f4fb5e8020bb
lambdamusic <michele.pasin@macbookpro99.home>
2019-05-02 14:09:36 +0100

    0.4.5

--- dimcli/VERSION.py
+++ dimcli/VERSION.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 #  -*- coding: UTF-8 -*-
 
-__version__ = "0.4.4.2"  # LATEST? =>  https://pypi.org/project/dimcli/
+__version__ = "0.4.5"  # LATEST? =>  https://pypi.org/project/dimcli/
 __copyright__ = "CopyRight (C) 2018 by Michele Pasin"
 __license__ = "Apache2.0"
 __author__ = "Michele Pasin"
