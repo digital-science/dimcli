3b9835b21478d41f2e2f83add5bbefdfa968a37b
lambdamusic <michele.pasin@gmail.com>
2019-05-20 12:43:27 +0100

    docs magic

--- dimcli/jupyter/magics.py
+++ dimcli/jupyter/magics.py
@@ -117,12 +117,56 @@ class DslMagics(Magics):
 
 
     @line_magic
-    def dsl_schema(self, line):
-        "Wrapper around the dsl describe function"
-        if self.connection:
-            return self._handle_query("describe schema")
-        else:
+    def dsl_docs(self, line):
+        """DimCli Magic
+        Wrapper around the dsl `describe` function - outputs live documentation for a source or entity
+        """
+        if not self.connection:
             print("Please login first: %dsl_login")
+            return
+        try:
+            import pandas as pd
+            df = pd.DataFrame()
+        except:
+            print("Sorry this functionality requires the Pandas python library. Please install it first")
+            return
+
+        obj = line.strip()
+        if obj and obj not in G.entities() and obj not in G.sources():
+            sou = " - ".join([x for x in G.sources()])
+            ent = " - ".join([x for x in G.entities()])
+            print(f"Sorry can't recognize this object. Use one of the following: Sources=[{sou}] Entities=[{ent}] ")
+            return
+        
+        res = self._handle_query(f"describe schema") # same query for all requests (filtering done here)
+
+        if not obj:
+            docs_for = G.sources()
+            header = "sources"
+        elif obj in G.entities():
+            docs_for = [obj]
+            header = "entities"
+        elif obj in G.sources():
+            docs_for = [obj]
+            header = "sources"
+
+        d = {header: [], 'field': [], 'type': [], 'description':[], 'is_filter':[], 'is_entity': [],  'is_facet':[],}
+        for S in docs_for:
+            for x in sorted(res.json[header][S]['fields']):
+                d[header] += [S]
+                d['field'] += [x]
+                d['type'] += [res.json[header][S]['fields'][x]['type']]
+                d['description'] += [res.json[header][S]['fields'][x]['description']]
+                d['is_filter'] += [res.json[header][S]['fields'][x]['is_filter']]
+                d['is_facet'] += [res.json[header][S]['fields'][x].get('is_facet', False)]
+                d['is_entity'] += [res.json[header][S]['fields'][x].get('is_entity', False)]
+
+        return df.from_dict(d)
+
+
+
+
+
 
 
 ip = get_ipython()
