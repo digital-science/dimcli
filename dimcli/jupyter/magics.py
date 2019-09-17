ca45d6262bdb219759a5b1696884d91ea7653eb2
lambdamusic <michele.pasin@gmail.com>
2019-09-17 14:03:54 +0100

    simplify login logic

--- dimcli/jupyter/magics.py
+++ dimcli/jupyter/magics.py
@@ -21,7 +21,6 @@ class DslMagics(Magics):
             self.dslobject = Dsl(show_results=False)
             return True
         else:
-            print("Warning: you are not logged in. Please use `dimcli.login(username, password)` before querying.")
             return False
 
     # def _handle_login(self):
