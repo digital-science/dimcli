18d11f26ab824fb66a0b0137d8b6dc00cfba100c
lambdamusic <michele.pasin@gmail.com>
2023-02-17 12:15:52 +0000

    v1.0.1

--- dimcli/VERSION.py
+++ dimcli/VERSION.py
@@ -1,11 +1,11 @@
 # !/usr/bin/env python
 #  -*- coding: UTF-8 -*-
 
-__version__ = "0.9.9.1"  # LATEST? =>  https://pypi.org/project/dimcli/
-__copyright__ = "CopyRight (C) 2018-2022 by Digital Science"
+__version__ = "1.0.1"  # LATEST? =>  https://pypi.org/project/dimcli/
+__copyright__ = "CopyRight (C) 2018-2023 by Digital Science"
 __license__ = "MIT"
 __author__ = "Michele Pasin"
-__author_email__ = "michele dot pasin at gmail dot com"
+__author_email__ = "michele dot pasin at digital dash science dot com"
 
 USAGE = "dimcli [options]"
 VERSION = "v" + __version__
